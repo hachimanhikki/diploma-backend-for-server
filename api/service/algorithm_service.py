2b3bc9ef52868dedf2213cc5cdd12b7820d26cb6
hachimanhikki <almasaytenov@gmail.com>
2022-04-24 00:08:12 +0600

    improve algorithm

--- api/service/algorithm_service.py
+++ api/service/algorithm_service.py
@@ -4,9 +4,9 @@ from api.service.db_service import populate_workload, update_teacher_total_hours
 
 def calculate_workload():
     subjects = Subject.objects.all()
-    # for subject in subjects:
-    #     _calculate_hours(subject)
-    _calculate_hours(subjects[6])
+    for subject in subjects:
+        _calculate_hours(subject)
+    #_calculate_hours(subjects[6])
     # print([(i, subjects[i]) for i in range(len(subjects))])
 
 
@@ -23,7 +23,7 @@ def _calculate_hours(subject: Subject) -> None:
     # remainder goes to the last teacher
     one_teacher_hour = ((subject.total_hour // teachers_count) // 10) * 10
     remainder = subject.total_hour - teachers_count * one_teacher_hour
-    one_teacher_groups = groups_count // teachers_count
+    # one_teacher_groups = groups_count // teachers_count
 
     # remain_hours is remaining hours to distribute
     # hours is total hour assigning to the teacher
@@ -31,6 +31,7 @@ def _calculate_hours(subject: Subject) -> None:
     remain_hours = [one_teacher_hour] * teachers_count
     remain_hours[-1] += remainder
     hours = remain_hours.copy()
+    print(remain_hours, hours)
     # prac_teacher_groups = [one_teacher_groups] * teachers_count
 
     # lec_teacher_groups, prac_teacher_groups не правильно используются
@@ -43,23 +44,22 @@ def _calculate_hours(subject: Subject) -> None:
         groups_count, lecture_count, teachers_count)
 
     # Compute lecture hours, lab hours, office hours, practice hours
-    _calculate_all_hours(remain_hours, subject, one_teacher_groups)
-
+    _calculate_all_hours(remain_hours, subject, prac_teacher_groups, hours)
+    print('@@@@@@@@', remain_hours)
     # practice_count is remaining practice lessons to distibure
-    practice_count = subject.practice_count - \
-        (one_teacher_groups * teachers_count)
+    # practice_count = subject.practice_count - \
+    #    (one_teacher_groups * teachers_count)
 
     # Distrubute remaining practice lessons
-    _calculate_remaining_hours(remain_hours, hours, subject,
-                               prac_teacher_groups, practice_count)
-
+    # _calculate_remaining_hours(remain_hours, hours, subject,
+    #                           prac_teacher_groups, practice_count)
     update_teacher_total_hours(teachers, hours)
     populate_workload(teachers, prac_teacher_groups,
                       lec_teacher_groups, groups)
 
 
 def _distribute_prac_teachers(groups_count: int, teachers_count: int) -> list:
-    return _distribute_groups_teachers(groups_count, teachers_count)
+    return _distribute_groups_teachers(groups_count, teachers_count)[::-1]
 
 
 def _distribute_lecture_teachers(groups_count: int, lecture_count: int, teachers_count: int) -> list:
@@ -74,7 +74,7 @@ def _distribute_groups_teachers(count_1: int, count_2: int, count_3: int = 0) ->
     return result
 
 
-def _calculate_all_hours(remain_hours: list, subject: Subject, one_teacher_groups: int) -> None:
+def _calculate_all_hours(remain_hours: list, subject: Subject, prac_teacher_groups: list, hours: list) -> None:
     subject_lecture_count = subject.lecture_count
     subject_lab_count = subject.lab_count
     for i in range(len(remain_hours)):
@@ -85,7 +85,8 @@ def _calculate_all_hours(remain_hours: list, subject: Subject, one_teacher_group
             remain_hours[i] -= subject.lab_hour
             subject_lab_count -= 1
         remain_hours[i] -= subject.office_hour
-        remain_hours[i] -= subject.practice_hour * one_teacher_groups
+        remain_hours[i] -= subject.practice_hour * prac_teacher_groups[i]
+        hours[i] += abs(remain_hours[i])
 
 
 def _calculate_remaining_hours(remain_hours: list, hours: list, subject: Subject, teacher_groups: list, practice_count: int) -> None:
