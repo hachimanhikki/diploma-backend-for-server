38e895281a4ea2fb86d49d102f1e2ddf29eca324
hachimanhikki <almasaytenov@gmail.com>
2022-05-22 21:24:56 +0600

    registration response fixed

--- accounts/views.py
+++ accounts/views.py
@@ -21,7 +21,7 @@ class CustomAuthToken(ObtainAuthToken):
             'username': user.username,
             'token': token.key,
             'email': user.email,
-            'department': user.department.name
+            'department_name': user.department.name
         })
 
 
@@ -32,12 +32,14 @@ def registration_view(request):
         data = {}
         if serializer.is_valid():
             teacher = serializer.save()
-            data['response'] = 'succesfully registered a new user'
-            data['email'] = teacher.email
             data['first_name'] = teacher.first_name
             data['second_name'] = teacher.second_name
+            data['kpi'] = teacher.kpi
+            data['username'] = teacher.username
+            data['email'] = teacher.email
             token = Token.objects.get(user=teacher).key
             data['token'] = token
+            data['department_name'] = teacher.department.name
         else:
             data = serializer.errors
         return Response(data)
